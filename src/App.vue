4b0b1d5881aba20952a91922c9deeb82cb07c206
Eiichi Uchibori <uchibori.mercurygroup@gmail.com>
2022-09-09 10:43:05 +0900

    axiosの追加とaxiosでのJSONファイル取得

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <script>
 import TreeChart from "vue-tree-chart-3";
+import axios from "axios";
 
 export default {
   name: 'app',
@@ -13,44 +14,14 @@ export default {
   data() {
     return {
       landscape: [],
-      treeData: {
-        name: 'root',
-        image_url: "https://dummyimage.com/150x150/ccc/999.png&text=root",
-        children: [
-          {
-            name: 'children1',
-            image_url: "https://dummyimage.com/150x150/ccc/999.png&text=children1"
-          },
-          {
-            name: 'children2',
-            image_url: "https://dummyimage.com/150x150/ccc/999.png&text=children2",
-            mate: [{
-              name: 'mate',
-              image_url: "https://dummyimage.com/150x150/ccc/999.png&text=mate"
-            }],
-            children: [
-              {
-                name: 'grandchild',
-                image_url: "https://dummyimage.com/150x150/ccc/999.png&text=grandchild"
-              },
-              {
-                name: 'grandchild2',
-                image_url: "https://dummyimage.com/150x150/ccc/999.png&text=grandchild2"
-              },
-              {
-                name: 'grandchild3',
-                image_url: "https://dummyimage.com/150x150/ccc/999.png&text=grandchild3"
-              }
-            ]
-          },
-          {
-            name: 'children3',
-            image_url: "https://dummyimage.com/150x150/ccc/999.png&text=children3"
-          }
-        ]
-      }
+      treeData: {}
     }
   },
+  mounted() {
+    axios.get('./data/treedata.json')
+    .then(response => this.treeData = response.data)
+    .catch(error => console.log(error))
+  }
 }
 </script>
 
